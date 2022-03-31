5824dc56172af756bb5a3c32be1fa97ba6d1c563
D'Angello <garcia.dangello@gmail.com>
2022-03-31 10:38:59 -0500

    Correción de base de datos

--- config/db.js
+++ config/db.js
@@ -3,24 +3,20 @@ const Sequelize = require('sequelize')
 const characterModel = require('../models/characters.model')
 const movieModel = require('../models/movies.model')
 const genreModel = require('../models/genre.model')
-const charMovModel = require('../models/character_movies.model')
 
-const sequelize = new Sequelize('bzanngjl', 'bzanngjl', 'RpJNORDknFuecqBcmoz_uSUL8qKW8exq', {
-    host: 'raja.db.elephantsql.com',
+const sequelize = new Sequelize('logbntgo', 'logbntgo', 'tRnmTF0Io21BeOicqadqQjUCOZxTXbDu', {
+    host: 'ruby.db.elephantsql.com',
     dialect: 'postgres'
 })
 
 const Character = characterModel(sequelize, Sequelize)
 const Movie = movieModel(sequelize, Sequelize)
 const Genre = genreModel(sequelize, Sequelize)
-const CharacterMovie = charMovModel(sequelize, Sequelize, Movie, Character)
 
-Genre.hasMany(Movie, {
-    foreignKey: 'genreId'
-})
+Genre.hasMany(Movie)
 Movie.belongsTo(Genre)
-Movie.belongsToMany(Character, { through: CharacterMovie });
-Character.belongsToMany(Movie, { through: CharacterMovie });
+Movie.belongsToMany(Character, { through: 'Character_Movies' });
+Character.belongsToMany(Movie, { through: 'Character_Movies' });
 
 sequelize.sync({alter: true})
     .then(() => {
