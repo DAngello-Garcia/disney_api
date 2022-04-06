d18e7e020600bd51e1593dc2069011b3cd46e9f5
D'Angello <garcia.dangello@gmail.com>
2022-04-06 13:08:37 -0500

    Creación de controlador y middleware de autenticación

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,2 +1,28 @@
 const jwt = require('jsonwebtoken')
+const _ = require('lodash')
+require('dotenv').config()
 
+async function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (_.isEmpty(authHeader)) {
+        return res.status(401).json({
+            error: "Unauthorized access"
+        })
+    }
+
+    const bearerToken = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(bearerToken, process.env.JWT_SECRET)
+        req.body.id = payload.id
+        req.body.username = payload.username
+        return next()
+    } catch (error) {
+        return res.status(403).json({
+            error: "Access forbidden", error: error.message
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
